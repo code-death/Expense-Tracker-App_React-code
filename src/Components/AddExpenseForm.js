4fac9a92ef30cdca4c8cce57c6d7b110f609ba3d
code-death <poorvanshkavta938@gmial.com>
2022-07-03 23:58:40 +0530

    Added adding expense functionality

--- src/Components/AddExpenseForm.js
+++ src/Components/AddExpenseForm.js
@@ -1,14 +1,55 @@
+import { useState, useContext } from 'react'
+import { AppContext } from '../Context/AppContext'
+import { v4 as uuidv4 } from 'uuid'
+
 const AddExpenseForm = props => {
+  const { dispatch } = useContext(AppContext)
+  const [name, setName] = useState('')
+  const [cost, setCost] = useState('')
+
+  const submitHandler = event => {
+    event.preventDefault()
+    const expense = {
+      name: name,
+      cost: cost,
+      id: uuidv4(),
+    }
+
+    dispatch({
+      type: 'ADD_EXPENSE',
+      payload: expense,
+    })
+  }
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='row'>
         <div className='col-sm mt-3'>
           <label htmlFor='name'>Name</label>
-          <input type='text' required className='form-control mt-2' id='name' />
+          <input
+            type='text'
+            required
+            className='form-control mt-2'
+            id='name'
+            onChange={event => {
+              setName(event.target.value)
+            }}
+            value={name}
+          />
         </div>
         <div className='col-sm mt-3'>
           <label htmlFor='Cost'>Cost</label>
-          <input type='text' required className='form-control mt-2' id='Cost' />
+          <input
+            value={cost}
+            type='number'
+            min='0'
+            required
+            className='form-control mt-2'
+            id='Cost'
+            onChange={event => {
+              setCost(event.target.value)
+            }}
+          />
         </div>
         <div className='col-sm mt-3'>
           <button type='submit' className='btn btn-primary'>
--- src/Context/AppContext.js
+++ src/Context/AppContext.js
@@ -3,13 +3,19 @@ import { createContext, useReducer } from 'react'
 const initialState = {
   budget: 2000,
   expenses: [
-    { id: 1, name: 'Shopping', cost: '50' },
-    { id: 2, name: 'Holiday', cost: '300' },
+    { id: 1, name: 'Shopping', cost: 50 },
+    { id: 2, name: 'Holiday', cost: 300 },
   ],
 }
 
 const AppReducer = (state, action) => {
   switch (action.type) {
+    case 'ADD_EXPENSE':
+      return {
+        ...state,
+        expenses: [...state.expenses, action.payload],
+      }
+
     default:
       return state
   }
